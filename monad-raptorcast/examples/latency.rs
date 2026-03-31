4c11c0f0b0cb626e34d0bcb737083b377730b2b1
Dmitry Shulyak <yashulyak@gmail.com>
2026-03-31 07:49:59 -0400

    raptorcast: require authenticated UDP socket
    Change-Id: Iee934c8680f2d4cffbc174cab7258e98b559075f

--- monad-raptorcast/examples/latency.rs
+++ monad-raptorcast/examples/latency.rs
@@ -668,7 +668,7 @@ fn setup_node(
         create_raptorcast_config(keypair_arc),
         SecondaryRaptorCastModeConfig::None,
         tcp_socket,
-        Some((authenticated_socket, auth_protocol)),
+        (authenticated_socket, auth_protocol),
         None,
         non_authenticated_socket,
         dataplane_control,
