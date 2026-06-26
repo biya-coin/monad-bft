2a052895d70164c14c9dc67da94cf546af9ab587
cyyuz <404613291@qq.com>
2026-06-26 17:20:40 +0800

    更新start.sh

--- start.sh
+++ start.sh
@@ -38,8 +38,6 @@ nohup ./scripts/monad-stress-bench.sh monad a >"$LOG_DIR/monad-a.log" 2>&1 &
 
 wait_for_socket ".monad/monad-a/mempool.sock" "monad-a mempool"
 
-nohup ./scripts/monad-stress-bench.sh rpc >"$LOG_DIR/rpc.log" 2>&1 &
-
 sleep 5
 
 nohup ./scripts/monad-stress-bench.sh monad b >"$LOG_DIR/monad-b.log" 2>&1 &
@@ -48,4 +46,16 @@ nohup ./scripts/monad-stress-bench.sh monad c >"$LOG_DIR/monad-c.log" 2>&1 &
 
 nohup ./scripts/monad-stress-bench.sh monad d >"$LOG_DIR/monad-d.log" 2>&1 &
 
-echo "biyachaind/monad/rpc 已后台启动，日志目录: $LOG_DIR"
+wait_for_socket ".monad/monad-b/mempool.sock" "monad-b mempool"
+wait_for_socket ".monad/monad-c/mempool.sock" "monad-c mempool"
+wait_for_socket ".monad/monad-d/mempool.sock" "monad-d mempool"
+
+nohup ./scripts/monad-stress-bench.sh rpc a >"$LOG_DIR/rpc-a.log" 2>&1 &
+
+nohup ./scripts/monad-stress-bench.sh rpc b >"$LOG_DIR/rpc-b.log" 2>&1 &
+
+nohup ./scripts/monad-stress-bench.sh rpc c >"$LOG_DIR/rpc-c.log" 2>&1 &
+
+nohup ./scripts/monad-stress-bench.sh rpc d >"$LOG_DIR/rpc-d.log" 2>&1 &
+
+echo "biyachaind/monad/rpc x4 已后台启动，日志目录: $LOG_DIR"
